f107282d2295031a385418ae75cf97e7cb250054
Matthew Lonis <mattlonis@gmail.com>
2026-07-27 20:23:33 -0400

    Add Capcitor XCode project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mrlonis.idlerpg',
+  appName: 'Idle RPG',
+  webDir: 'dist/idle-rpg/browser',
+};
+
+export default config;
